8e85639701495ffc821c214beda24c8bc419e807
Robbie Bendick <robbiebendick@gmail.com>
2021-09-15 20:37:12 -0700

    style arena cut-offs

--- src/components/Cutoffs/Cutoffs.jsx
+++ src/components/Cutoffs/Cutoffs.jsx
@@ -13,16 +13,9 @@ function Cutoffs({rankOne, glad, duelist, teamRatings}) {
             for (let i = 0; i < rankOne.length; i++) {
                 if(teamRatings[i] >= rankOne[i]){
                     r1 = true;
-                    if (i === 0) {
-                        r1Twos = true;
-                    }
-                    if (i === 1) {
-                        r1Threes = true;
-                    }
-                    if (i === 2) {
-                        r1Fives = true;
-                    }
-                    
+                    if (i === 0) r1Twos = true;
+                    if (i === 1) r1Threes = true;
+                    if (i === 2) r1Fives = true;
                 }
             }
         }
@@ -40,17 +33,19 @@ function Cutoffs({rankOne, glad, duelist, teamRatings}) {
 
         <motion.div initial={{"opacity":0}} animate={{"opacity": 1}} transition={{"duration": 0.8}}>
             <h3 className="cutoffs">Arena Cutoffs:</h3>
-            <table className="cutoff-table table-hover">
-                <th>Titles</th>
-                <th>2v2</th>
-                <th>3v3</th>
-                <th>5v5</th>
-                <tbody>
-                <CutoffTableRow title="Rank One" twos={rankOne[0]} threes={rankOne[1]} fives={rankOne[2]} />
-                <CutoffTableRow title="Gladiator" twos={glad[0]} threes={glad[1]} fives={glad[2]} />
-                <CutoffTableRow title="Duelist" twos={duelist[0]} threes={duelist[1]} fives={duelist[2]} />
-                </tbody>
-            </table>
+            <div className="arena-cutoff-table-container">
+                <table className="cutoff-table table-hover">
+                    <th>Titles</th>
+                    <th>2v2</th>
+                    <th>3v3</th>
+                    <th>5v5</th>
+                    <tbody>
+                    <CutoffTableRow title="Rank One" twos={rankOne[0]} threes={rankOne[1]} fives={rankOne[2]} />
+                    <CutoffTableRow title="Gladiator" twos={glad[0]} threes={glad[1]} fives={glad[2]} />
+                    <CutoffTableRow title="Duelist" twos={duelist[0]} threes={duelist[1]} fives={duelist[2]} />
+                    </tbody>
+                </table>
+            </div>
         </motion.div>
     )
 }
